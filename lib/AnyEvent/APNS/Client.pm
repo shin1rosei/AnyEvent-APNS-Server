faf43621ef6ef239f28348b7978601e6ed55c23c
Shinichiro Sei <sei@kayac.com>
2013-02-21 17:29:11 +0900

    first commit

---
+++ lib/AnyEvent/APNS/Client.pm
@@ -0,0 +1,75 @@
+package AnyEvent::APNS::Client;
+
+use strict;
+use warnings;
+
+use AnyEvent::MPRPC::Client;
+use Log::Minimal;
+
+use Mouse;
+
+has host => (
+    is       => 'ro',
+    required => 1,
+);
+
+has port => (
+    is       => 'ro',
+    required => 1,
+);
+
+has client => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        AnyEvent::MPRPC::Client->new({
+            host => $self->host,
+            port => $self->port,
+        });
+    },
+);
+
+sub notify {
+    my ($self, $device_token, $msg) = @_;
+    critf("message not set") unless $msg;
+
+    my $cv = $self->client->call('notify' => {
+        token => $device_token,
+        msg   => $msg,
+    });
+
+    my $res;
+    eval { $res = $cv->recv};
+    if (my $error = $@) {
+        critf "apns send error";
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
+
+=head1 NAME
+
+AnyEvent::APNS::Client -
+
+=head2 SYNOPSIS
+
+  use AnyEvent::APNS::Client;
+
+  AnyEvent::APNS::Client->new({
+     host => 'server-host',
+     port => 8888,
+  });
+
+  $client->notify(
+     $device_token, #HEX
+     $msg,
+  );
+
+=head1 DESCRIPTION
+
+AnyEvent::APNS::Client is client module for AnyEvent::APNS::Server.
+
--- lib/AnyEvent/APNS/Server.pm
+++ lib/AnyEvent/APNS/Server.pm
@@ -3,7 +3,182 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
-1;
+use utf8;
+use Encode;
+
+use Mouse;
+use Log::Minimal;
+
+use AnyEvent::APNS;
+
+use Cache::LRU;
+use Try::Tiny;
+
+has certificate => (
+    is => 'ro',
+);
+
+has private_key => (
+    is => 'ro',
+);
+
+has is_sandbox => (
+    is => 'ro',
+);
+
+has port => (
+    is => 'ro',
+);
+
+has sent_token => (
+    is      => 'rw',
+    default => sub {
+        Cache::LRU->new(size => 10000);
+    },
+);
+
+has message_queue => (
+    is      => 'rw',
+    default => sub {[];},
+);
+
+has apns => (
+    is      => 'rw',
+    clearer => 'close_apns',
+);
+
+has on_error_response => (
+    is      => 'rw',
+    isa     => 'CodeRef',
+    default => sub { sub { warn @_ } },
+);
+
+has _last_connect_at => (
+    is => 'rw',
+);
+
+has _last_send_at => (
+    is => 'rw',
+);
+
+no Any::Moose;
+
+sub run {
+    my $self = shift;
+
+    my $cv = AnyEvent->condvar;
+
+    my $server = AnyEvent::MPRPC::Server->new(
+        port        => $self->port,
+        on_error    => sub {},
+        on_accept   => sub {
+            infof "[mprpc server] on_accept";
+            $self->_last_send_at(time);
+        },
+        on_dispatch => sub {
+            infof "[mprpc server] on_dispatch";
+        },
+
+    );
+    $server->reg_cb(
+        notify => sub {
+            my ($res_cv, $params) = @_;
+
+            my $p = $params->[0];
+            infof "[mprpc server] send notify";
+
+            if ($self->apns && $self->apns->connected) {
+                my $identifier = $self->apns->send(pack("H*", $p->{token}), {
+                    aps => {
+                        alert => decode_utf8($p->{msg}),
+                    },
+                });
+                $self->sent_token->set($identifier, $p->{token});
+
+                infof "[mprpc server] send notify complete %s", $p->{token};
+            }
+            else {
+                infof "[apns] push queue";
+                push @{$self->message_queue}, $p; #つなぎ終わったら送信
+                $self->_connect_to_apns;
+            }
+
+            $res_cv->result('ok');
+        },
+    );
+
+    my $t; $t = AnyEvent->timer( 
+        # 最終送信日時から１分以上たっていたらAPNSとの接続を破棄する
+        after    => 60,
+        interval => 60,
+        cb    => sub {
+            if ($self->apns) {
+                if (time - $self->_last_send_at > 60) {
+                    try {$self->close_apns};
+                    infof "[apns] close apns";
+                }
+            }
+        },
+    );
+
+    $cv->recv;
+}
+
+sub _connect_to_apns {
+    my $self = shift;
+
+    return if ($self->apns);
+
+    $self->apns(AnyEvent::APNS->new(
+        certificate => $self->certificate,
+        private_key => $self->private_key,
+        sandbox     => $self->is_sandbox,
+        on_error    => sub {
+            my ($handle, $fatal, $message) = @_;
+
+            my $t; $t = AnyEvent->timer(
+                after    => 0,
+                interval => 10,
+                cb       => sub {
+                    undef $t;
+                    infof "[apns] reconnect";
+                    $self->_last_connect_at(time);
+                    $self->apns->connect;
+                },
+            );
+
+            # 即座に再接続
+            infof "[apns] error fatal: $fatal message: $message";
+        },
+        on_connect  => sub {
+            warnf "[apns] on_connect";
+            $self->_last_connect_at(time);
+
+            if (@{$self->message_queue}) { #未送信メッセージがあれば送信する
+                while (my $q = shift @{$self->message_queue}) {
+                    $self->apns->send(pack("H*", $q->{token}), {
+                        aps => {
+                            alert => decode_utf8($q->{msg}),
+                        },
+                    });
+                    infof "[apns] send from queue ".$q->{token};
+                }
+            }
+        },
+        on_error_response => sub {
+            my ($identifier, $state) = @_;
+
+            if ($state == 8) { #不正なtoken
+                my $token = $self->sent_token->get($identifier) || undef;
+                $self->on_error_response($token, @_);
+            }
+        },
+    ));
+    $self->apns->connect;
+}
+
+__PACKAGE__->meta->make_immutable;
+
 __END__
 
 =head1 NAME
@@ -13,6 +188,13 @@ AnyEvent::APNS::Server -
 =head1 SYNOPSIS
 
   use AnyEvent::APNS::Server;
+
+  AnyEvent::APNS::Server->new({
+    is_sandbox  => 1 or 0,
+    certificate => <certificate string>,
+    private_key => <key_file string>,
+    port        => 8888,
+  })->run;
 
 =head1 DESCRIPTION
 
